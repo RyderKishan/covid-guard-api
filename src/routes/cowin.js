cd655e7df69c7de02045bd6ea3fcc6c1d36fbc2a
RyderKishan <kishan020696@gmail.com>
2021-05-14 17:30:19 +0530

    addded then block

--- src/routes/cowin.js
+++ src/routes/cowin.js
@@ -3,14 +3,19 @@ const { get } = require('../utils/request');
 exports.bind = (router) => {
   router.get('/cowin/states', async (req, res) => {
     // try {
-      const path = '/admin/location/states';
-      const response = await get(`${process.env.COVID_API}${path}`);
-      res.status(200).json(response);
+    const path = '/admin/location/states';
+    get(`${process.env.COVID_API}${path}`)
+      .then((response) => {
+        res.status(200).json(response);
+      })
+      .catch((err) => {
+        console.log('err', err);
+      });
     // } catch (e) {
-      // res.status(500).json({
-      //   status: 500,
-      //   message: JSON.stringify(e),
-      // });
+    // res.status(500).json({
+    //   status: 500,
+    //   message: JSON.stringify(e),
+    // });
     // }
   });
 };
