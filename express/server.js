683b347e3f5ee7440a6cd1548ae7d3770614fd9b
RyderKishan <kishan020696@gmail.com>
2021-05-14 11:35:30 +0530

    Netlify Git Refleect

--- express/server.js
+++ express/server.js
@@ -1,27 +1,15 @@
 'use strict';
 const express = require('express');
+const path = require('path');
 const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
-const fetch = require('node-fetch');
-
-const API_ENDPOINT = 'https://icanhazdadjoke.com/';
 
 const router = express.Router();
 router.get('/', (req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html' });
   res.write('<h1>Hello from Express.js!</h1>');
   res.end();
-});
-router.get('/call', (req, res) => {
-  fetch(API_ENDPOINT, { headers: { Accept: 'application/json' } })
-    .then((response) => response.json())
-    .then((data) => {
-      res.status(200).json(data);
-    })
-    .catch((error) => {
-      res.status(500).json({ error: String(error) });
-    });
 });
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
 router.post('/', (req, res) => res.json({ postBody: req.body }));
@@ -45,7 +33,7 @@ router.get('/health', (req, res) => {
 });
 
 app.use(bodyParser.json());
-app.use('/.netlify/functions/server', router); // path must route to lambda
+app.use('/.netlify/functions/server', router);  // path must route to lambda
 app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
 
 module.exports = app;
