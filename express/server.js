ad31f0492b8af8ac34fd7d5d2ea1ad003c9472f5
RyderKishan <kishan020696@gmail.com>
2021-05-14 10:10:21 +0530

    Serverless Http

---
+++ express/server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+const os = require('os');
+const serverless = require('serverless-http');
+const app = express();
+const bodyParser = require('body-parser');
+
+const router = express.Router();
+router.get('/', (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.write('<div>Covid Guard Api</div>');
+  res.end();
+});
+router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
+router.post('/', (req, res) => res.json({ postBody: req.body }));
+router.get('/health', (req, res) => {
+  res.status(200).json({
+    message: 'Application UP!',
+    containerName: os.hostname(),
+    timeStamp: new Date()
+  });
+});
+
+app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(morgan('tiny'));
+
+app.use('/.netlify/functions/server', router); // path must route to lambda
+app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
+
+module.exports = app;
+module.exports.handler = serverless(app);
--- src/config/index.js
+++
@@ -1,7 +0,0 @@
-const config = {
-  restapi: process.env.REST_API || 'https://jsonplaceholder.typicode.com',
-  serverPort: process.env.PORT || 7400,
-  authenticationStrategy: 'google'
-};
-
-module.exports = config;
