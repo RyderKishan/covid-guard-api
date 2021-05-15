29a3af9120191ccbf04cb96c1bed2bda36dde0d9
RyderKishan <kishan020696@gmail.com>
2021-05-15 13:33:55 +0530

    Added Token

--- src/utils/request.js
+++ src/utils/request.js
@@ -28,13 +28,13 @@ const ErrorObject = async (response) => {
 
 const commonHeaders = {
   'content-type': 'application/json',
-  'accept-encoding': 'gzip, deflate, br',
   'accept-language': 'en_US',
-  // host: 'covid-guard-api-prod.us-east-1.elasticbeanstalk.com',
-  accept:
-    'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+  origin: 'https://selfregistration.cowin.gov.in',
+  referer: 'https://selfregistration.cowin.gov.in/',
+  authorization: `Bearer ${process.env.TOKEN || ''}`,
   'user-agent':
     'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36',
+  // host: 'covid-guard-api-prod.us-east-1.elasticbeanstalk.com',
 };
 
 const get = async (endpoint, headers, options = {}) => {
